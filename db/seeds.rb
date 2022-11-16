275c431055c4197ec206db9e089a289ad8416877
Jon Entwistle <jonny3107@aol.com>
2022-11-16 16:27:52 +0000

    challenge complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create([{name: "Tao", address: "London", phone_number: "07828026051", category: "chinese" }, {name: "Sebastien's", address: "London", phone_number: "07828026051", category: "italian"}, {name: "Bento", address: "London", phone_number: "07828026051", category: "japanese"}, {name: "La Baguette", address: "London", phone_number: "07828026051", category: "french"}, {name: "Belgo", address: "London", phone_number: "07828026051", category: "belgian"}])
